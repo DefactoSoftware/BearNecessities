e26c5f25fa76272942044e5fa385be9f3a84d388
Matthijs Kuiper <info@matthijskuiper.nl>
2019-04-15 13:10:15 +0200

    terrain_class and sprite_class functions

--- apps/bear_necessities_web/lib/bear_necessities_web/views/playfield.ex
+++ apps/bear_necessities_web/lib/bear_necessities_web/views/playfield.ex
@@ -1,3 +1,19 @@
 defmodule BearNecessitiesWeb.Playfield do
   use BearNecessitiesWeb, :view
+
+  defp terrain_class(tile) do
+    case tile do
+      :nothing -> "nothing"
+      _ -> "grass"
+    end
+  end
+
+  defp sprite_class(tile, player_id) do
+    case tile do
+      %Bear{id: id, direction: direction} when id == player_id -> "bear self #{direction}"
+      %Bear{} -> "bear opponent"
+      %Tree{} -> "tree"
+      _ -> nil
+    end
+  end
 end
