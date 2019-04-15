393410f8c94e9f06bf45c394348d2be662c3604e
FatboyPunk <marcel.horlings@gmail.com>
2019-04-15 17:36:32 +0200

    move database setup to not _web

--- apps/bear_necessities/config/prod.exs
+++ apps/bear_necessities/config/prod.exs
@@ -3,3 +3,9 @@
 # and only for organization purposes. All other config goes to
 # the umbrella root.
 use Mix.Config
+
+# Configure your database
+config :bear_necessities, BearNecessities.Repo,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
--- apps/bear_necessities_web/config/prod.exs
+++ apps/bear_necessities_web/config/prod.exs
@@ -24,12 +24,6 @@ config :bear_necessities_web, BearNecessitiesWeb.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-# Configure your database
-config :bear_necessities, BearNecessities.Repo,
-  url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  ssl: true
-
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
