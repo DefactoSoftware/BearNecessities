e2b8022233928ebe1759973983cd83870d48a70a
FatboyPunk <marcel.horlings@gmail.com>
2019-04-18 15:27:36 +0200

    move the app to unbearable.nl

--- apps/bear_necessities_web/config/prod.exs
+++ apps/bear_necessities_web/config/prod.exs
@@ -16,7 +16,7 @@ use Mix.Config
 
 config :bear_necessities_web, BearNecessitiesWeb.Endpoint,
   http: [port: System.get_env("PORT")],
-  url: [scheme: "https", host: "bear-necessities.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "unbearable.nl", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
