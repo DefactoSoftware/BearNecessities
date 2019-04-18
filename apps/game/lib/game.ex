b873ad643b637d2cca8e97f4473e244af6976481
FatboyPunk <marcel.horlings@gmail.com>
2019-04-18 12:28:03 +0200

    show dad gif

--- apps/game/lib/game.ex
+++ apps/game/lib/game.ex
@@ -7,7 +7,7 @@ defmodule Game do
   @max_honey_drops 8
   @field_height 40
   @field_width 40
-  @miliseconds_dead_screen 1000
+  @miliseconds_dead_screen 2100
 
   defstruct [:field, :bears, :bees, :trees, :honey_drops]
 
